86369fb88a4e3d5ff450b090a49994936ee12446
Anar.N <an301@duke.edu>
2025-02-12 10:21:43 +0800

    restarting project for refactor

--- README.md
+++
@@ -1,2 +0,0 @@
-# pomogranate
-My pomodoro timer 
--- dist/preload.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const electron_1 = require("electron");
-electron_1.contextBridge.exposeInMainWorld('electron', {
-    send: (channel, data) => electron_1.ipcRenderer.send(channel, data),
-    receive: (channel, callback) => {
-        electron_1.ipcRenderer.on(channel, (_, data) => callback(data));
-    },
-});
